ee8b147a6e650ee3b1d8d195e9c3679ac9224479
Varun Rathi <varunrathi28@gmail.com>
2017-12-19 13:24:27 +0530

    Initial Commit with protocols added

---
+++ ViperPOC/Protocol/Protocols.swift
@@ -0,0 +1,31 @@
+//
+//  Protocols.swift
+//  ViperPOC
+//
+//  Created by Varun Rathi on 19/12/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+protocol ViewPresenterProtocol {
+    
+    func updateView()
+}
+
+protocol PresenterViewProtocol {
+    
+}
+
+protocol PresenterRouterProtocol {
+    
+}
+
+protocol PresenterInteracterProtocol {
+    
+}
+
+protocol InteractorPresenterProtocol {
+    
+}
+
---
+++ ViperPOC/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ViperPOC
+//
+//  Created by Varun Rathi on 19/12/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var presenter : ViewPresenterProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
